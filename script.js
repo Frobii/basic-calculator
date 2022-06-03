4552fa08e483a75701ee4d662baf35187d69eb1a
Frobi <frobi.webdev@outlook.com>
2022-06-03 12:50:42 +0930

    give-the-calculator-functionality

--- script.js
+++ script.js
@@ -32,13 +32,29 @@ function operate(operator, num1, num2) {
     }
 };
 
-const display = document.querySelector(".calcDisplay")
-let displayValue = "";
-
 function inputNumber(number) {
     displayValue += number;
     return display.innerText += number;
 }
+
+function operatorClear() {
+    if (display.innerText === "+") {
+        display.innerText = "";
+    } else if (display.innerText === "-") {
+        display.innerText = "";
+    } else if (display.innerText === "x") {
+        display.innerText = "";
+    } else if (display.innerText === "/") {
+        display.innerText = "";
+    } else if (display.innerText === "%") {
+        display.innerText = "";
+    }
+};
+
+const display = document.querySelector(".calcDisplay")
+let displayValue = "";
+let operation = "";
+let storedNumber = "";
 
 const zero = document.querySelector(".zero");
 const one = document.querySelector(".one");
@@ -54,41 +70,107 @@ const dot = document.querySelector(".dot");
 
 const clear = document.querySelector(".clear");
 
+const plus = document.querySelector(".add");
+const take = document.querySelector(".subtract");
+const times = document.querySelector(".multiply");
+const divided = document.querySelector(".divide");
+const difference = document.querySelector(".remainder");
+const equals = document.querySelector(".equals");
+// use different names here to avoid function calls
+
 zero.addEventListener("click", () => {
+    operatorClear();
     inputNumber(0);
 });
 one.addEventListener("click", () => {
+    operatorClear();
     inputNumber(1);
 });
 two.addEventListener("click", () => {
+    operatorClear();
     inputNumber(2);
 });
 three.addEventListener("click", () => {
+    operatorClear();
     inputNumber(3);
 });
 four.addEventListener("click", () => {
+    operatorClear();
     inputNumber(4);
 });
 five.addEventListener("click", () => {
+    operatorClear();
     inputNumber(5);
 });
 six.addEventListener("click", () => {
+    operatorClear();
     inputNumber(6);
 });
 seven.addEventListener("click", () => {
+    operatorClear();
     inputNumber(7);
 });
 eight.addEventListener("click", () => {
+    operatorClear();
     inputNumber(8);
 });
 nine.addEventListener("click", () => {
+    operatorClear();
     inputNumber(9);
 });
 dot.addEventListener("click", () => {
+    operatorClear();
     inputNumber(".");
 });
 
 clear.addEventListener("click", () => {
     displayValue = "";
     display.innerText = "";
+});
+
+plus.addEventListener("click", () => {
+    operation = "";
+    storedNumber = displayValue;
+    operation = add;
+    displayValue = "";
+    display.innerText = "+";
+});
+
+take.addEventListener("click", () => {
+    operation = "";
+    storedNumber = displayValue;
+    operation = subtract;
+    displayValue = "";
+    display.innerText = "-";
+});
+
+times.addEventListener("click", () => {
+    operation = "";
+    storedNumber = displayValue;
+    operation = multiply;
+    displayValue = "";
+    display.innerText = "x";
+});
+
+divided.addEventListener("click", () => {
+    operation = "";
+    storedNumber = displayValue;
+    operation = divide;
+    displayValue = "";
+    display.innerText = "/";
+});
+
+difference.addEventListener("click", () => {
+    operation = "";
+    storedNumber = displayValue;
+    operation = remainder;
+    displayValue = "";
+    display.innerText = "%";
+});
+
+equals.addEventListener("click", () => {
+    display.innerText = 
+    operate(operation, parseInt(storedNumber), parseInt(displayValue));
+    storedNumber = "";
+    displayValue = "";
 });
